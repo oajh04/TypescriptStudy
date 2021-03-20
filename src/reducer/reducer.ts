c2bbdf4f27180f64b349bb3af58f54cdfd346c55
oajh04 <cloudtubekr@naver.com>
2021-03-20 22:30:13 +0900

    redux

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -1,5 +1,5 @@
 import { Action } from '../Interfaces/Count';
-import { PLUS, MINUS } from './actionTypes';
+import { PLUS, MINUS, CUSTOM, ZERO } from './actionTypes';
 
 const initialState = {
     count: 0,
@@ -11,6 +11,10 @@ function reducer(state = initialState, action: Action) {
         return { ...state, count: state.count + 1 };
       case MINUS:
         return { ...state, count: state.count - 1 };
+      case CUSTOM:
+        return { ...state, count: state.count + action.payload };
+      case ZERO:
+        return { ...state, count: 0};
       default:
         return state;
     }
